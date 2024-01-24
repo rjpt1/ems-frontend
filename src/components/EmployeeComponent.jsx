b9c354a2dc5154023acc323da7d954320ac06037
rafael-pedraza <rjpt1@outlook.com>
2024-01-23 20:17:25 -0500

    router

---
+++ src/components/EmployeeComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EmployeeComponent = () => {
+    return (
+        <div>
+            crear
+        </div>
+    )
+}
+
+
+export default EmployeeComponent
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header className="navbar navbar-dark bg-primary">
+                <a className="navbar-brand" href="https://www.javaguides.net">Employee Managemen System</a>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
